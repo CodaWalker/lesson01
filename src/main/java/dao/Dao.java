f9212e42c5c439f3e1a901b682f92db383a47b6f
kasyanov_ks <fierymail@list.ru>
2019-08-09 16:47:40 +0300

    Добавил основные классы для функционирования приложения. В main прописал тестовое редактирование строк.

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import models.Auto;
+import models.User;
+import java.util.List;
+
+public interface Dao {
+    public User findById(int id);
+    public void save(User user);
+    public void update(User user);
+    public void delete(User user);
+    public Auto findAutoById(int id);
+    public List<User> findAll();
+}
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,49 @@
 package dao;
 
-public class UserDao {
+import models.Auto;
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateSessionFactoryUtil;
+import java.util.List;
+
+public class UserDao implements Dao{
+
+    public User findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public Auto findAutoById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
+    }
+
+    public List<User> findAll() {
+        //List<User> users = (List<User>) HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("from models.User").list();
+        // return users;
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().createQuery("from models.User", User.class).getResultList();
+    }
 }
